798a9b731127b9b14ddbc9c996ebdb1b3e0db99a
Mason Ng <mnaung@expediagroup.com>
2024-11-22 17:09:31 -0800

    springboot setup

---
+++ src/main/java/personal/mason/rtdtravelservice/RtdTravelServiceApplication.java
@@ -0,0 +1,13 @@
+package personal.mason.rtdtravelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtdTravelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtdTravelServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal/mason/rtdtravelservice/RtdTravelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.mason.rtdtravelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtdTravelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
